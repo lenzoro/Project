b2f7583c7d7f97d0f94be5be3f7828ad569351f6
AlphVenom <dario.mahon@gmail.com>
2019-05-17 14:13:02 -0400

    added users

--- client/main.js
+++ client/main.js
@@ -4,17 +4,31 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 import '../lib/collections.js';
-Template.mainbody.helpers({
-  tasks() {
+Template.task.helpers({
+  MainAll() {
     return tasksDB.find({});
   },
 });
 
+Accounts.ui.config({
+	passwordSignupFields: "USERNAME_ONLY",
+});
+
 Template.add.events({
-  'click js-add'(event, instance) {
-   var namepath = $("#namepath").val();
-   $("#namepath").val('');
-      console.log ("Test")
-      tasksDB.insert({"Task":taskpath, "createdon":new Date().getTime() , "postedby":Meteor.user()._id});
+  'change .js-add'(event) {
+	   var taskpath = $("#taskpath").val();
+	   $("#taskpath").val('');
+      	console.log ("Test");
+      	tasksDB.insert({"Task":taskpath, "createdon":new Date().getTime()}); //, "postedby":Meteor.user()._id});
   },
 });
+Template.task.events({
+	'click .js-delete'(event, instance) {
+	var taskID = this._id;
+		console.log (taskID);
+	$("#" + taskID).fadeOut("slow","swing", function(){
+		console.log ("hi");
+		tasksDB.remove({_id:taskID}); 	
+	});
+  },
+})
